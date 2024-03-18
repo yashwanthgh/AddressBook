2ece435b89e03950346048d5b2fb9939c919636b
yashwanthgh <yugnasura@gmail.com>
2024-03-18 13:48:14 +0530

    Added: functionality which takes details as input from the user

---
+++ ContactController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class ContactController
+    {
+        static ArrayList contactList = new ArrayList();
+
+        public void AddNewContact()
+        {
+            Console.WriteLine("Enter first name:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter last name:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter address :");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter city name:");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter state:");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter zip:");
+            string zip = Console.ReadLine();
+            Console.WriteLine("Enter phone number:");
+            string phoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter email:");
+            string emial = Console.ReadLine();
+            contactList.Add(new Contact(firstName,  lastName, address, city, state, zip, phoneNumber, emial));
+            Console.WriteLine($"Contact of {firstName} {lastName} added");
+        }
+
+    }
+}
