2dc1a69c1bdbfba9f5530e62e841149983d44cc0
yashwanthgh <yugnasura@gmail.com>
2024-03-18 20:11:29 +0530

    Added: Functionality of deleting contact using choices

--- AddressBookMain.cs
+++ AddressBookMain.cs
@@ -13,6 +13,7 @@ namespace AddressBook
             Console.WriteLine("Address Book!");
             Console.WriteLine("To add a new contact Press 1");
             Console.WriteLine("To edit contact Press 2");
+            Console.WriteLine("To delete contact Press 3");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
             {
@@ -23,6 +24,10 @@ namespace AddressBook
                 case 2:
                     ContactController editContact = new ContactController();
                     editContact.EditContact();
+                    break;
+                case 3:
+                    ContactController deleteContact = new ContactController();
+                    deleteContact.DeleteContact();
                     break;
             }
         }
