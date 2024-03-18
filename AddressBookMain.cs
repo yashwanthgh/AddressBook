46bb1fb8af2d8de965bf29011ba5cea03b78c08e
yashwanthgh <yugnasura@gmail.com>
2024-03-18 20:05:11 +0530

    Added: Functionality of editing exiting contact using choices

--- AddressBookMain.cs
+++ AddressBookMain.cs
@@ -12,12 +12,17 @@ namespace AddressBook
         {
             Console.WriteLine("Address Book!");
             Console.WriteLine("To add a new contact Press 1");
+            Console.WriteLine("To edit contact Press 2");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
             {
                 case 1:
-                    ContactController contactController = new ContactController();
-                    contactController.AddNewContact();
+                    ContactController addContact = new ContactController();
+                    addContact.AddNewContact();
+                    break;
+                case 2:
+                    ContactController editContact = new ContactController();
+                    editContact.EditContact();
                     break;
             }
         }
